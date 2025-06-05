5a4467ea5222b6ba418ae75f7679f2901dbe1867
HerbalMG <Herbalmg1@gmail.com>
2025-06-05 17:07:52 +0530

    basic setup done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./components/Footer"
+import AppRoutes from "./route/AppRoutes"
+
+function App() {
+
+  return (
+    <>
+      
+  <AppRoutes/>
+  <Footer/>
+    </>
+  )
+}
+
+export default App
